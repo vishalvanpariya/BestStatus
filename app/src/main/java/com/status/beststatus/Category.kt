40fdb4f860dc7ab020e3c50656ee38fc2034fa39
Vishal Vanpariya <vanpariyavishal02@gmail.com>
2019-11-17 10:18:36 +0530

    init

--- app/src/main/java/com/status/beststatus/Category.kt
+++ app/src/main/java/com/status/beststatus/Category.kt
@@ -1,11 +1,19 @@
 package com.status.beststatus
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.database.*
 import com.google.firebase.database.DataSnapshot
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_category.*
 import java.util.*
 
 
@@ -28,8 +36,31 @@ class Category : AppCompatActivity() {
                 for (ds in dataSnapshot.children) {
                     list.add(ds.key!!)
                 }
-
+                catrecycler.adapter=CatAdapter(context = this@Category,list = list)
+                catrecycler.layoutManager=LinearLayoutManager(this@Category)
             }
         })
+    }
+
+    class CatAdapter(var context: Context,var list: LinkedList<String>):RecyclerView.Adapter<CatAdapter.Holder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatAdapter.Holder {
+            return Holder(LayoutInflater.from(context).inflate(R.layout.categoryitem,parent,false))
+        }
+
+        class Holder(itemview:View): RecyclerView.ViewHolder(itemview){
+            lateinit var text:TextView
+            init {
+                text=itemview.findViewById(R.id.categorytext)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: CatAdapter.Holder, position: Int) {
+            holder.text.text=list[position]
+        }
+
     }
 }
